66e6fe33d1502f7b0bb78b84bcce1a0e750445ed
Ali Khorsand <ali.khorsand@live.com>
2018-11-14 21:15:04 +0330

    Home Component and Register Components are implemented

---
+++ DatingApp-Spa/src/app/register/register.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from './../services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  model: any = {};
+  @Output() cancelRegister = new EventEmitter();
+
+  constructor(
+    private auth: AuthService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.auth.register(this.model).subscribe(() => {
+      console.log('registeration successful');
+    }, error => {
+      console.log('error');
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log('canceled');
+  }
+
+}
